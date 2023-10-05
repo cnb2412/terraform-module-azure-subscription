61fe908b13d62393eb76dd0fcc9cb0985552803c
Christian Niephaus <christian@cnb24.de>
2023-10-05 21:07:59 +0200

    fixed array issue with billing section

--- resources.subscription.tf
+++ resources.subscription.tf
@@ -1,4 +1,4 @@
 resource "azurerm_subscription" "sub" {
   subscription_name = var.subscription_name
-  billing_scope_id  = data.azurerm_billing_mca_account_scope.main[0].id # array due to count
+  billing_scope_id  = data.azurerm_billing_mca_account_scope.main.id # array due to count
 }
